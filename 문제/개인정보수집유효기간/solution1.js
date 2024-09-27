e9ae0141891b5c640c0735144e4b789c19b87b28
BIROOM <zoon1031@naver.com>
2024-09-27 13:19:32 +0900

    개인정보수집유효기간 문제

---
+++ 문제/개인정보수집유효기간/solution1.js
@@ -0,0 +1,28 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/150370?language=javascript
+function solution(today, terms, privacies) {
+  const MAX = 28;
+  console.log(28 * 12);
+  var answer = [];
+  return answer;
+}
+
+console.log(
+  solution(
+    "2022.05.19",
+    ["A 6", "B 12", "C 3"],
+    ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+  )
+); //	[1, 3]
+// console.log(
+//   solution(
+//     "2020.01.01",
+//     ["Z 3", "D 5"],
+//     [
+//       "2019.01.01 D",
+//       "2019.11.15 Z",
+//       "2019.08.02 D",
+//       "2019.07.01 D",
+//       "2018.12.28 Z",
+//     ]
+//   )
+// ); // [1, 4]
